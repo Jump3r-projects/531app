65c839245fd86786f61d3420885ce9adda097e64
Jump3r-projects <37297118+Jump3r-projects@users.noreply.github.com>
2019-04-16 16:13:15 +1000

    build commit

---
+++ build/precache-manifest.adf8c561d62a0a9a08f745957736be19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "724ad64bca57eec77e95",
+    "url": "/static/js/main.724ad64b.chunk.js"
+  },
+  {
+    "revision": "f6433f80cb396d890ae1",
+    "url": "/static/js/1.f6433f80.chunk.js"
+  },
+  {
+    "revision": "724ad64bca57eec77e95",
+    "url": "/static/css/main.683de163.chunk.css"
+  },
+  {
+    "revision": "f6433f80cb396d890ae1",
+    "url": "/static/css/1.f12605bc.chunk.css"
+  },
+  {
+    "revision": "e402bc40efbc9058317033208f710fa9",
+    "url": "/index.html"
+  }
+];
